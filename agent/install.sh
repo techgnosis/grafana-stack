b4ea88822276573999ed3db7765084a670bbaf06
James Musselwhite <james.musselwhite@grafana.com>
2021-07-03 23:54:35 -0700

    combined install script

---
+++ agent/install.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+kapp deploy -a grafana-agent-logs \
+-f <(kubectl create secret generic loki-creds \
+-n grafana-labs \
+--from-literal=LOKI_HOSTNAME=${LOKI_HOSTNAME} \
+--dry-run=client \
+-o yaml) \
+-f <(kubectl create secret generic gem-creds \
+-n grafana-labs \
+--from-literal=GEM_URL=${GEM_URL} \
+--from-literal=GEM_USERNAME=${GEM_USERNAME} \
+--from-literal=GEM_PASSWORD=${GEM_PASSWORD} \
+--dry-run=client \
+-o yaml) \
+-f <(kubectl create secret generic tempo-creds \
+-n grafana-labs \
+--from-literal=TEMPO_ENDPOINT=${TEMPO_ENDPOINT} \
+--dry-run=client \
+-o yaml) \
+-f cm.yml \
+-f ds.yml \
+-f namespace.yml
