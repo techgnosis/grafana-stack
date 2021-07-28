752e08278ac2ed65829f352beabb508cbb0895a2
James Musselwhite <james.musselwhite@grafana.com>
2021-07-28 15:27:21 -0700

    separating out into namespaces

--- grafana-enterprise/install.sh
+++ grafana-enterprise/install.sh
@@ -2,13 +2,14 @@
 
 set -euo pipefail
 
-kapp deploy -a grafana-enterprise-env \
+kapp deploy -a grafana-enterprise \
 -f certificate.yml \
--f grafana-ldap.yml
+-f grafana-ldap.yml \
+-f datasources.yml
 
 helm upgrade --install grafana-enterprise grafana \
 --repo https://grafana.github.io/helm-charts \
 --version 6.13.5 \
---namespace grafana-labs \
+--namespace grafana-enterprise \
 --values grafana-values.yml \
 --wait
--- minio/install.sh
+++ minio/install.sh
@@ -6,6 +6,5 @@ helm upgrade --install minio minio \
 --repo https://helm.min.io \
 --version 8.0.9 \
 --namespace minio \
---create-namespace \
 --values values.yml \
 --wait
