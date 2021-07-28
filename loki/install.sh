03361e8a3bb86b74b033ac4310b15bc58790df52
James Musselwhite <james.musselwhite@grafana.com>
2021-07-28 10:59:32 -0700

    loki to loki ns

--- loki/install.sh
+++ loki/install.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 helm upgrade --install loki loki-distributed \
 --repo https://grafana.github.io/helm-charts \
 --version 0.35.0 \
---namespace grafana-labs \
+--namespace loki \
 --values loki-values.yml \
 --wait
