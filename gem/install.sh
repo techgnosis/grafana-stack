f35280739864f67713a339d384c90c8a7f08b375
James Musselwhite <james.musselwhite@grafana.com>
2021-07-29 14:53:15 -0700

    gem with external config file

--- gem/install.sh
+++ gem/install.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 helm upgrade --install gem enterprise-metrics \
 --repo https://grafana.github.io/helm-charts \
 --version 1.3.4 \
---namespace grafana-labs \
+--namespace gem \
 --values gem-values.yml \
 --set-file 'license.contents=../licenses/gem-license.jwt' \
+--values config.yml \
 --wait
