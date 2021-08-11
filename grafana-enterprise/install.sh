e11f7ac0aa0489147714de22882fd38d374140d9
James Musselwhite <james.musselwhite@grafana.com>
2021-08-11 15:57:19 -0700

    adding the license automatically

--- grafana-enterprise/install.sh
+++ grafana-enterprise/install.sh
@@ -5,7 +5,12 @@ set -euo pipefail
 kapp deploy -a grafana-enterprise \
 -f certificate.yml \
 -f grafana-ldap.yml \
--f datasources.yml
+-f datasources.yml \
+-f <(k create cm license \
+--namespace grafana-enterprise \
+--from-file=license.jwt=../licenses/ge-license.jwt \
+--dry-run=client \
+-o yaml)
 
 helm upgrade --install grafana-enterprise grafana \
 --repo https://grafana.github.io/helm-charts \
