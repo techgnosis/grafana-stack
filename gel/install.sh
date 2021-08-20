7a72f91d64e3315667a9f75c6cea131afc948f60
James Musselwhite <james.musselwhite@grafana.com>
2021-08-20 12:08:49 -0700

    using the correct path for the WIP GEL Helm chart

--- gel/install.sh
+++ gel/install.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-helm upgrade --install enterprise-logs-test-fixture /home/james/code/christian/helm-charts/charts/enterprise-logs \
+helm upgrade --install enterprise-logs-test-fixture /home/james/code/helm-charts/charts/enterprise-logs \
 --version 0.1.0 \
 --namespace gel \
 --set-file 'license.contents=../licenses/gel-license.jwt'
