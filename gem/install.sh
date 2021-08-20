4c3d981c964c4799ebf92a6f0e0a6401c9029545
James Musselwhite <james.musselwhite@grafana.com>
2021-08-20 14:55:47 -0700

    values file rename for consistency

--- gem/install.sh
+++ gem/install.sh
@@ -6,6 +6,6 @@ helm upgrade --install gem enterprise-metrics \
 --repo https://grafana.github.io/helm-charts \
 --version 1.4.7 \
 --namespace gem \
---values gem-values.yml \
+--values values.yml \
 --set-file 'license.contents=../licenses/gem-license.jwt' \
 --values config.yml
