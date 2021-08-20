1eda3ee0159a453741005d6de5f15b3ecc632d1a
James Musselwhite <james.musselwhite@grafana.com>
2021-08-20 14:12:28 -0700

    need to disable anti-affinity for ingesters since i only have one node

--- gel/install.sh
+++ gel/install.sh
@@ -5,4 +5,5 @@ set -euo pipefail
 helm upgrade --install enterprise-logs-test-fixture /home/james/code/helm-charts/charts/enterprise-logs \
 --version 0.1.0 \
 --namespace gel \
+--values values.yml \
 --set-file 'license.contents=../licenses/gel-license.jwt'
