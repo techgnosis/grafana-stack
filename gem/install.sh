36b5effa264892fe7a7124055b1f0db54f8e6407
James Musselwhite <james.musselwhite@grafana.com>
2021-09-01 15:23:31 -0700

    upgraded to 1.5

--- gem/install.sh
+++ gem/install.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 helm upgrade --install gem enterprise-metrics \
 --repo https://grafana.github.io/helm-charts \
---version 1.4.7 \
+--version 1.5.0 \
 --namespace gem \
 --values values.yml \
 --set-file 'license.contents=../licenses/gem-license.jwt' \
