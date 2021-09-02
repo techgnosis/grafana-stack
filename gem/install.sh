964242fe4a55d518201d0a6e7891088b19c586d4
James Musselwhite <james.musselwhite@grafana.com>
2021-09-02 12:12:19 -0700

    adding ingress to GEM so I can send node_exporter from my laptop

--- gem/install.sh
+++ gem/install.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+kapp deploy -a gem \
+-f certificate.yml \
+-f ingress.yml
+
 helm upgrade --install gem enterprise-metrics \
 --repo https://grafana.github.io/helm-charts \
 --version 1.5.0 \
