b3fa4253e9b47aec0b029c17babd9180c8425f23
James Musselwhite <james.musselwhite@grafana.com>
2021-09-08 15:32:58 -0700

    adding ingress for GEL so I can test things easier with the API

--- gel/install.sh
+++ gel/install.sh
@@ -2,8 +2,12 @@
 
 set -euo pipefail
 
+kapp deploy -a gel \
+-f certificate.yml \
+-f ingress.yml
+
 helm upgrade --install enterprise-logs-test-fixture /home/james/code/helm-charts/charts/enterprise-logs \
---version 0.1.0 \
+--version 1.0.0 \
 --namespace gel \
 --values values.yml \
 --set-file 'license.contents=../licenses/gel-license.jwt'
