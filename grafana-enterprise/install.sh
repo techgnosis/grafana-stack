6e69de5ff1d4325f4831b55e29eb0e788f67fc3c
James Musselwhite <james.musselwhite@grafana.com>
2021-09-27 10:38:25 -0700

    grafana back to HA. SMTP added

--- grafana-enterprise/install.sh
+++ grafana-enterprise/install.sh
@@ -14,7 +14,7 @@ kapp deploy -a grafana-enterprise \
 
 helm upgrade --install grafana-enterprise grafana \
 --repo https://grafana.github.io/helm-charts \
---version 6.16.4 \
+--version 6.16.6 \
 --namespace grafana-enterprise \
 --values grafana-values.yml \
 --values grafana-ini.yml \
