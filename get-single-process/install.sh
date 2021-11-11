d25038ac78ad372794b3b14a1eba0b19694c8464
James Musselwhite <james.musselwhite@grafana.com>
2021-11-10 19:28:37 -0600

    adding manifests for GET

---
+++ get-single-process/install.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+kapp deploy -a get \
+-f namespace.yaml \
+-f <(kubectl create secret generic get-license \
+--namespace get \
+--from-file ../licenses/get-license.jwt \
+--dry-run=client \
+-o yaml) \
+-f <(kubectl create configmap get-config \
+--namespace get \
+--from-file config.yaml \
+--dry-run=client \
+-o yaml) \
+-f get-service.yaml \
+-f get-ss.yaml \
+-f job.yaml \
+-f minio.yaml \
+-f certificate.yaml \
+-f ingress.yaml \
+-f compactor-ss.yaml \
+-f compactor-service.yaml \
+-f gateway-deployment.yaml \
+-f gateway-service.yaml \
+-f alertmanager-service.yaml \
+-f alertmanager-ss.yaml
