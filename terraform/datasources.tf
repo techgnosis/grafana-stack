133faebcb7174e5a24cb399505becf2d813aeb78
James Musselwhite <james.musselwhite@grafana.com>
2021-12-01 09:54:15 -0800

    adding postgres datasource so i can demo recorded queries

--- terraform/datasources.tf
+++ terraform/datasources.tf
@@ -10,6 +10,22 @@ resource "grafana_data_source" "Frontend" {
   url  = "https://frontend.lab"
 }
 
+resource "grafana_data_source" "GrafanaPostgres" {
+  type = "postgres"
+  name = "Grafana Database"
+  database_name = "grafana-enterprise"
+  url  = "postgres:5432"
+  username = "grafana-enterprise"
+  secure_json_data {
+    password = "grafana-enterprise"
+  }
+  json_data {
+    ssl_mode = "disable"
+    postgres_version = "1200"
+  }
+  
+}
+
 resource "grafana_data_source_permission" "Backend" {
   datasource_id = grafana_data_source.Backend.id
   permissions {
