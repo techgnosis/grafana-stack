5c1768d2f55dae6d374e8b2e53475e2d7b976a9f
James Musselwhite <james.musselwhite@grafana.com>
2021-12-08 23:49:17 -0800

    simplify the grafana resources

--- terraform/datasources.tf
+++ terraform/datasources.tf
@@ -10,20 +10,28 @@ resource "grafana_data_source" "Frontend" {
   url  = "https://frontend.lab"
 }
 
-resource "grafana_data_source" "GrafanaPostgres" {
-  type = "postgres"
-  name = "Grafana Database"
-  database_name = "grafana-enterprise"
-  url  = "postgres:5432"
-  username = "grafana-enterprise"
-  secure_json_data {
-    password = "grafana-enterprise"
-  }
-  json_data {
-    ssl_mode = "disable"
-    postgres_version = "1200"
-  }
-  
+resource "grafana_data_source" "InterlakeMetrics" {
+  type = "prometheus"
+  name = "Interlake Metrics"
+  url  = "http://gem-enterprise-metrics-gateway.gem.svc.cluster.local:8080/prometheus"
+  basic_auth_enabled = true
+  basic_auth_username = "interlake"
+  basic_auth_password = "aW50ZXJsYWtlLWFsbC1hbGw6ezFgJjo1PSs1dTc3RnQ+KDkyODc5Iy5V"
+}
+
+resource "grafana_data_source" "InterlakeLogs" {
+  type = "loki"
+  name = "Interlake Logs"
+  url = "http://gateway.gel.svc.cluster.local:3100"
+  basic_auth_enabled = true
+  basic_auth_username = "interlake"
+  basic_auth_password = "aW50ZXJsYWtlLWFsbC1hbGw6JXsxWzJkfjhjMSU3WWZ7fTE0XTEyJSU3"
+}
+
+resource "grafana_data_source" "InterlakeTraces" {
+  type = "tempo"
+  name = "Interlake Traces"
+  url = "http://tempo.tempo.svc.cluster.local:3100"
 }
 
 resource "grafana_data_source" "RecordedQueries" {
