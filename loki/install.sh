bab1e0343564a0c8117335a9a6aacbd8b089d18b
James Musselwhite <james.musselwhite@grafana.com>
2022-02-04 16:11:30 -0800

    switched loki to statefulset

--- loki/install.sh
+++ loki/install.sh
@@ -5,7 +5,9 @@ set -euo pipefail
 kapp deploy -a loki \
 -f namespace.yaml \
 -f certificate.yml \
--f deployment.yaml \
+-f statefulset.yaml \
+-f services.yaml \
+-f pvc.yaml \
 -f ingress.yml  \
 -f <(kubectl create configmap loki-config \
 --namespace loki \
