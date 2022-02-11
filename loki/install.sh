8530cf3894a9b72c9356b652d72d9574e789b421
James Musselwhite <james.musselwhite@grafana.com>
2022-02-10 17:10:22 -0800

    adding an agent for loki

--- loki/install.sh
+++ loki/install.sh
@@ -9,6 +9,15 @@ kapp deploy -a loki \
 -f services.yaml \
 -f pvc.yaml \
 -f ingress.yml  \
+-f agent/clusterrolebinding.yaml \
+-f agent/deployment.yaml \
+-f agent/service.yaml \
+-f agent/serviceaccount.yaml \
+-f <(kubectl create configmap grafana-agent-config \
+--namespace loki \
+--from-file=agent/agent.yaml \
+--dry-run=client \
+-o yaml) \
 -f <(kubectl create configmap loki-config \
 --namespace loki \
 --from-file=loki.yaml \
