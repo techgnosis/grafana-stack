27f613bf18a2614ea90efe96de36bd017c5c4ffe
James Musselwhite <james.musselwhite@grafana.com>
2022-02-23 12:49:32 -0800

    loki derived field works in terraform. mysql has the correct password field

--- terraform/datasources.tf
+++ terraform/datasources.tf
@@ -20,10 +20,32 @@ resource "grafana_data_source" "Loki" {
   type = "loki"
   name = "Loki"
   url = "http://loki.loki.svc.cluster.local:3100"
+
+  json_data {
+
+    derived_field {
+      matcher_regex = ".*(?:trace_id\":\\s*\")(.*?)(?:\"|\\s).*"
+      name = "trace_id"
+      url = "$${__value.raw}"
+      datasource_uid = grafana_data_source.Tempo.uid
+    }
+  }
 }
 
 resource "grafana_data_source" "Tempo" {
   type = "tempo"
   name = "Tempo"
   url = "http://tempo.tempo.svc.cluster.local:3100"
+}
+
+resource "grafana_data_source" "MySQL" {
+  type = "mysql"
+  name = "MySQL Grafana DB"
+  url = "mysql:3306"
+  database_name = "grafana-enterprise"
+  username = "grafana-enterprise"
+
+  secure_json_data {
+    password = "grafana-enterprise"
+  }
 }
