41f1acba3feec1a80222944e4b12ee2e48ff71cd
James Musselwhite <james.musselwhite@grafana.com>
2022-03-07 11:04:59 -0800

    alertmanager looks OK

--- alertmanager/install.sh
+++ alertmanager/install.sh
@@ -5,5 +5,10 @@ set -euo pipefail
 kapp deploy -a alertmanager \
 -f namespace.yaml \
 -f certificate.yml \
--f alertmanager.yaml \
--f ingress.yml
+-f deployment.yaml \
+-f ingress.yml \
+-f <(kubectl create cm alertmanager \
+--namespace alertmanager \
+--from-file=alertmanager.yaml \
+--dry-run=client \
+-o yaml) \
