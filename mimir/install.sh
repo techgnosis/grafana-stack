9acc51f23e5979fa6e5bc7196a050ba8ef892569
James Musselwhite <james.musselwhite@grafana.com>
2022-03-30 12:41:57 -0700

    mimir first draft

---
+++ mimir/install.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+kapp deploy -a mimir \
+-f namespace.yaml \
+-f certificate.yml \
+-f statefulset.yaml \
+-f services.yaml \
+-f pvc.yaml \
+-f ingress.yml  \
+-f <(kubectl create configmap mimir-config \
+--namespace mimir \
+--from-file=mimir.yaml=./mimir.yaml \
+--dry-run=client \
+-o yaml) #\
+#-f agent/clusterrolebinding.yaml \
+#-f agent/deployment.yaml \
+#-f agent/service.yaml \
+#-f agent/serviceaccount.yaml \
+#-f <(kubectl create configmap grafana-agent-config \
+#--namespace loki \
+#--from-file=agent/agent.yaml \
+#--dry-run=client \
+#-o yaml) \
