031c1c05ea1bc280cbc7fa34639291918bc1db00
James Musselwhite <james.musselwhite@grafana.com>
2022-05-25 11:29:49 -0700

    wip reorg

--- wip/loki-helm/install.sh
+++ wip/loki-helm/install.sh
@@ -7,9 +7,9 @@ kapp deploy -a loki \
 -f certificate.yaml \
 -f ingress.yaml
 
-helm upgrade --install loki loki \
+helm upgrade --install loki loki-distributed \
 --repo https://grafana.github.io/helm-charts \
---version 2.9.0 \
+--version 0.48.3 \
 --namespace loki \
 --values values.yaml \
 --wait
