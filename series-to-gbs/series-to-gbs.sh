0c0c29aaabbb25744f4fecb7e91681a9b5a8fbd4
James Musselwhite <james.musselwhite@grafana.com>
2022-08-26 11:16:52 -0700

    more formatting

--- series-to-gbs/series-to-gbs.sh
+++ series-to-gbs/series-to-gbs.sh
@@ -8,6 +8,9 @@ fi
 
 ACTIVE_SERIES="${1}"
 
+echo "ESTABLISHING BASELINE NUMBERS"
+echo "-----------------------------"
+
 wget --quiet http://localhost:12345/metrics
 
 BYTES=$(stat --format=%s metrics)
@@ -20,13 +23,23 @@ echo "SERIES FROM NODE_EXPORTER: ${SERIES}"
 
 BYTES_PER_SERIES=$((BYTES / SERIES))
 
-echo "BYTES PER SERIES: ${BYTES_PER_SERIES}"
+echo "BYTES PER NODE_EXPORTER SERIES: ${BYTES_PER_SERIES}"
 
-TOTAL_BYTES=$((ACTIVE_SERIES * BYTES_PER_SERIES * 60 * 24 * 31))
+
+
+echo ""
+echo "MATH FOR ${ACTIVE_SERIES} SERIES"
+echo "----------------------------"
+
+BYTES_PER_SCRAPE=$((ACTIVE_SERIES * BYTES_PER_SERIES))
+
+echo "BYTES PER SCRAPE: ${BYTES_PER_SCRAPE}"
+
+TOTAL_BYTES=$((BYTES_PER_SCRAPE * 60 * 24 * 31))
 
 echo "TOTAL BYTES PER MONTH: ${TOTAL_BYTES}"
 
 TOTAL_GIBS=$((TOTAL_BYTES / 1073741824))
 
-echo "TOTAL GiBs per month: ${TOTAL_GIBS}"
+echo "TOTAL GiBs PER MONTH: ${TOTAL_GIBS}"
 
