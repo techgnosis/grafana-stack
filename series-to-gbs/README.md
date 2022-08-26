ad84242dc8d644f2f22c9932e6270f9d91fbcf67
James Musselwhite <james.musselwhite@grafana.com>
2022-08-26 10:44:02 -0700

    script to convert series to gbs

---
+++ series-to-gbs/README.md
@@ -0,0 +1,5 @@
+Tools and techniques for converting active series to GiBs
+
+Assuming 1 minute scrape interval
+
+
---
+++ series-to-gbs/count-series.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+if [ -f metrics ]; then
+    rm metrics
+fi
+
+ACTIVE_SERIES="${1}"
+
+wget --quiet http://localhost:12345/metrics
+
+BYTES=$(stat --format=%s metrics)
+
+echo "BYTES: ${BYTES}"
+
+SERIES=$(cat metrics | awk '!/^#/' | wc -l)
+
+echo "SERIES: ${SERIES}"
+
+BYTES_PER_SERIES=$((BYTES / SERIES))
+
+echo "BYTES PER SERIES: ${BYTES_PER_SERIES}"
+
+TOTAL_BYTES=$((ACTIVE_SERIES * BYTES_PER_SERIES * 60 * 24 * 31))
+
+echo "TOTAL BYTES PER MONTH: ${TOTAL_BYTES}"
+
+TOTAL_GIBS=$((TOTAL_BYTES / 1073741824))
+
+echo "TOTAL GiBs: ${TOTAL_GIBS}"
+
