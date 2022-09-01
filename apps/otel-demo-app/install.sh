a0275db14790b661c55f241e64efe6e515c55385
James Musselwhite <james.musselwhite@grafana.com>
2022-09-01 16:48:56 -0700

    adding grafana agent to collect otel-demo logs

--- apps/otel-demo-app/install.sh
+++ apps/otel-demo-app/install.sh
@@ -8,4 +8,13 @@ kapp deploy -a opentelemetry-demo \
 -f namespace.yaml \
 -f full-manifest.yaml \
 -f ingress.yaml \
--f certificate.yaml
+-f certificate.yaml \
+-f agent/deployment.yaml \
+-f agent/clusterrolebinding.yaml \
+-f agent/service.yaml \
+-f agent/serviceaccount.yaml \
+-f <(kubectl create configmap grafana-agent-config \
+--namespace hotrod \
+--from-file agent/agent.yaml \
+--dry-run=client \
+-o yaml)
