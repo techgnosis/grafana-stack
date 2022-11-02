3485936322765c35a0f75cd3d3ed7242334b89ac
James Musselwhite <james.musselwhite@grafana.com>
2022-11-02 13:54:27 -0700

    stuff

--- wip/gem-helm/clean.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-mc rm clusterminio/enterprise-metrics-admin --recursive --force
-mc rm clusterminio/enterprise-metrics-tsdb --recursive --force
-mc rm clusterminio/enterprise-metrics-ruler --recursive --force
-mc rm clusterminio/enterprise-metrics-alertmanager --recursive --force
--- wip/gem-helm/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-set -euo pipefail
-
-helm uninstall gem -n gem --wait
-
-kapp delete -a gem
